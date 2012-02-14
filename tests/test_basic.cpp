638b40378518203e27cb88febedfd2ca2047147d
Francisco Claude <fclaude@recoded.cl>
2012-02-13 20:18:58 -0800

    messed up the licence, now it's fixed.

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -12,7 +12,7 @@ are permitted provided that the following conditions are met:
       this list of conditions and the following disclaimer in the documentation
       and/or other materials provided with the distribution.
 
-    * Neither the name of Zend Technologies USA, Inc. nor the names of its
+    * Neither the name of libcds. nor the names of its
       contributors may be used to endorse or promote products derived from this
       software without specific prior written permission.
 
@@ -28,7 +28,6 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ********************************************************************************/
-
 
 #include <gtest/gtest.h>
 #include <pthread.h>

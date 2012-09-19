17936f8580ffd0ef4ad6bc329f9b0a06c984ab26
Roberto Konow <rkonow@ratonera-laptop.(none)>
2012-09-18 23:09:04 -0400

    Added Mapper interface and MapperNone as a dummy implementation

---
+++ src/immutable/mappernone.h
@@ -0,0 +1,62 @@
+/********************************************************************************
+Copyright (c) 2012, Francisco Claude,Roberto Konow
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+    * Neither the name of libcds nor the names of its
+      contributors may be used to endorse or promote products derived from this
+      software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+********************************************************************************/
+
+#ifndef SRC_IMMUTABLE_MAPPERNONE_H_
+#define SRC_IMMUTABLE_MAPPERNONE_H_
+
+#include <libcds/libcds.h>
+#include <libcds/io.h>
+#include <libcds/immutable/mapper.h>
+
+namespace cds {
+namespace immutable {
+
+using std::ostream;
+using std::istream;
+
+/** Mapper that doesn't change the value (identity)
+ *
+ *  @author Francisco Claude
+ */
+class MapperNone : public Mapper {
+  public:
+    MapperNone();
+    virtual ~MapperNone() {}
+    virtual cds_word Map(const cds_word s) const;
+    virtual cds_word Unmap(const cds_word s) const;
+    virtual cds_word GetSize() const;
+    virtual void Save(ostream &out) const;
+    static MapperNone *Load(istream &input);
+};
+};
+};
+
+#endif  // SRC_IMMUTABLE_MAPPERNONE_H_
